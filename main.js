0447238dce7bbc65309a3173c566db57ed3db945
Steve Domenico <stratcat944@gmail.com>
2015-06-30 16:30:50 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// Document ready
+
+$(document).on('ready', function() {
+
+// Div variables
+
+var outerShell 		$('<div class="outer-shell"></div>');
+
+var innerShell 		$('<div class="inner-shell"></div>');
+
+var clockScreen 	$('<div class="clock-screen"></div>');
+
+var indicatorAmPm 	$('<div class="ampm-indicator">*</div>');
+
+var clockText		$('<div class="clock-text">15:16</div>');
+
+var amFreqLabel		$('<div class="am-freqlabel">AM</div>');
+
+var pmFreqLabel		$('<div class="pm-freqlabel">PM</div>');
+
+
+// appends
+
+$(body).append(outerShell);
+
+$(outerShell).append(inner-shell);
+
+$(inner-shell).append(clock-screen);
+
+$(inner-shell).append(ampm-indicator);
+
+$(inner-shell).append(clock-text);
+
+$(inner-shell).apend(am-freqlabel);
+
+$(inner-shell).append(pm-freqlabel);
+
+
+
+
+
+
+  
+});
