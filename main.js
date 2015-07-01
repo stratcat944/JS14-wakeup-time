9f018bb82e611f752ad9b8b17f26441d0fd0f282
Steven Domenico <stratcat944@Xenu-3.local>
2015-06-30 22:45:05 -0600

    fixed inner container

--- main.js
+++ main.js
@@ -1,44 +1,22 @@
-// Document ready
-
 $(document).on('ready', function() {
 
-// Div variables
-
-var outerShell 		$('<div class="outer-shell"></div>');
-
-var innerShell 		$('<div class="inner-shell"></div>');
-
-var clockScreen 	$('<div class="clock-screen"></div>');
-
-var indicatorAmPm 	$('<div class="ampm-indicator">*</div>');
-
-var clockText		$('<div class="clock-text">15:16</div>');
-
-var amFreqLabel		$('<div class="am-freqlabel">AM</div>');
-
-var pmFreqLabel		$('<div class="pm-freqlabel">PM</div>');
-
-
-// appends
-
-$('.container').append(outer-shell);
-
-$(outerShell).append(inner-shell);
-
-$(inner-shell).append(clock-screen);
-
-$(inner-shell).append(ampm-indicator);
-
-$(inner-shell).append(clock-text);
-
-$(inner-shell).apend(am-freqlabel);
-
-$(inner-shell).append(pm-freqlabel);
+	var innerShell = $('<div class="innerShell"> </div>');
+	var leftbuttonAM = $('<div class="leftLabel"> </div>');
+	var rightbuttonPM = $('<div class="rightLabel"> </div>');
+	var clockScreen = $('<div class="clockScreen"> </div>');
+	var clockText = $('<div class="clockText">12:30</div>');
+	var bottomLabelAM = $('<div class="bottomAm">AM</div>');
+	var bottomLabelPM = $('<div class="bottomPm">PM</div>'); 
 
 
 
+	$('.container').append(innerShell);
+	$(innerShell).append(leftbuttonAM);
+	$(innerShell).append(rightbuttonPM);
+	$(innerShell).append(clockScreen);
+	$(clockScreen).append(clockText);
+	$(leftbuttonAM).append(bottomLabelAM);
+	$(rightbuttonPM).append(bottomLabelPM);
 
 
-
-  
 });
